fa66f14e0d7cb97e0d6675205de2dc860731ae02
Mark Andrews <mjandrews.org@gmail.com>
2024-05-16 14:08:19 +0100

    Update scripts

--- scripts/script2.R
+++ scripts/script2.R
@@ -9,3 +9,21 @@ M_10 <- glm(doctorco ~ age,
 )
 
 summary(M_10)
+confint.default(M_10)
+
+estimates <- coef(M_10)
+
+# log of the average number of visits if age = 25
+estimates[1] + estimates[2] * 25
+
+# the average number of visits if age = 25
+exp(estimates[1] + estimates[2] * 25)
+
+# the average number of visits if age = 75
+exp(estimates[1] + estimates[2] * 75)
+
+doctor_df2 <- tibble(age = seq(20, 80))
+
+predictions1 <- add_predictions(doctor_df2, M_10)
+
+ggplot(predictions1, aes(x = age, y = pred)) + geom_point() + geom_line()
